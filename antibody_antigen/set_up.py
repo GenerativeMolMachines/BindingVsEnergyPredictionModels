367a0cfb679e333405e2974bca15d5049c5b709e
djzhendogg <djzhendogg.is.busy2@gmail.com>
2025-10-15 16:20:45 +0300

    fix df antibody-antigen

--- antibody_antigen/set_up.py
+++ antibody_antigen/set_up.py
@@ -1,6 +1,6 @@
 import torch
 from tape import TAPETokenizer, ProteinBertModel
-from src.config import BERT_PATH
 
+BERT_PATH = "/antibody_antigen_model/pretrain_bert.models"
 model = ProteinBertModel.from_pretrained('bert-base')
 torch.save(model, BERT_PATH)
